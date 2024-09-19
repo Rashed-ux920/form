3c5b651d1497f35352ee84de586748535be6cb74
rashed <125754285+j22003@users.noreply.github.com>
2024-09-19 16:04:40 +0300

    hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+/* eslint-disable no-undef */
+// import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App(){
+  const [formData,setFormData] = useState({
+    name: '',
+    age: '',
+    email: ''
+  })
+  const onchangehandler = (event)=>{
+    setFormData(()=>(
+      {
+        ...formData,
+        [event.target.name] : event.target.value 
+      }
+    ))
+  }
+  const handelsubmit = (e) => {
+      e.preventDefault();
+      console.log(
+        formData
+      )
+  }
+  return(
+    <form onSubmit={handelsubmit}>
+      
+      <input type='text' name='name'onChange={onchangehandler} placeholder='your name'></input>
+      <br/>
+      <br/>
+      <input type='number' name='age' onChange={onchangehandler} placeholder='your age'></input>
+      <br/>
+      <br/>
+      <input type='email' name='email' onChange={onchangehandler} placeholder='your email'></input>
+
+      <br/>
+      <br/>
+      <button type='submit'>send</button>
+
+    </form>
+      // <>
+      //       <h1>text</h1>
+      //       <input onChange={onchangehandler} name='text'></input>
+      // </>
+  )
+
 }
 
 export default App;
